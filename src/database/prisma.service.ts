f5b16f90666542f688adfc283e32916c35b4a595
Isaac <aziksimpson@gmail.com>
2026-06-04 11:07:28 +0300

    run linters

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,32 +1,32 @@
-import { inject, injectable } from "inversify";
-import { TYPES } from "../types.js";
-import { ILogger } from "../logger/logger.interface.js";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient as PrismaClientT } from "../generated/prisma/internal/class.js";
-import { PrismaClient } from "../generated/prisma/client.js";
+import { inject, injectable } from 'inversify';
+import { TYPES } from '../types.js';
+import { ILogger } from '../logger/logger.interface.js';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaClient as PrismaClientT } from '../generated/prisma/internal/class.js';
+import { PrismaClient } from '../generated/prisma/client.js';
 
 @injectable()
 export class PrismaService {
-  client: PrismaClientT;
+	client: PrismaClientT;
 
-  constructor(@inject(TYPES.ILogger) private logger: ILogger) {
-    const adapter = new PrismaBetterSqlite3({
-          url: process.env.DATABASE_URL ?? "file:./dev.db",
-        });
+	constructor(@inject(TYPES.ILogger) private logger: ILogger) {
+		const adapter = new PrismaBetterSqlite3({
+			url: process.env.DATABASE_URL ?? 'file:./dev.db',
+		});
 
-    this.client = new PrismaClient({ adapter });
-  }
+		this.client = new PrismaClient({ adapter });
+	}
 
-  async connect(): Promise<void> {
-    try {
-      await this.client.$connect();
-      this.logger.log("Connected to database successfully");
-    } catch (e) {
-      this.logger.error("Error connecting to database", e);
-    }
-  }
+	async connect(): Promise<void> {
+		try {
+			await this.client.$connect();
+			this.logger.log('Connected to database successfully');
+		} catch (e) {
+			this.logger.error('Error connecting to database', e);
+		}
+	}
 
-  async disconnect(): Promise<void> {
-    await this.client.$disconnect();
-  }
+	async disconnect(): Promise<void> {
+		await this.client.$disconnect();
+	}
 }
